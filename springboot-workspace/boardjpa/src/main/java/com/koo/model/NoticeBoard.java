8500f6d609112aa9f65d862b8b07294364f30891
xlzbthxyjkoo <happyyj01@naver.com>
2024-09-12 17:01:46 +0900

    linux

--- springboot-workspace/boardjpa/src/main/java/com/koo/model/NoticeBoard.java
+++ springboot-workspace/boardjpa/src/main/java/com/koo/model/NoticeBoard.java
@@ -1,16 +1,21 @@
 package com.koo.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -38,5 +43,6 @@ public class NoticeBoard {
 	
 	@ManyToOne
 	private BoardUser writer;
-	
+
+
 }
